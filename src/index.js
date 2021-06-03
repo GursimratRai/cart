fd6f40125e0de2cfb9ca1b11401c5337c27af229
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-06-03 15:32:05 +0530

    Setting up firebase (database)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from './firebase'
 
+ var firebaseConfig = {
+    apiKey: "AIzaSyCJOJkWXzlQJ_y3TnXQUwR1SaElpdLpurA",
+    authDomain: "cart-57545.firebaseapp.com",
+    projectId: "cart-57545",
+    storageBucket: "cart-57545.appspot.com",
+    messagingSenderId: "25027836946",
+    appId: "1:25027836946:web:4235b40ad04bb26c7a2ccb"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
